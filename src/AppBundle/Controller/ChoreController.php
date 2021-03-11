4ac8c40b3c7812e739282853500809abee831ae0
DatSwissGuy <kxosz9@outlook.com>
2021-03-11 15:53:24 +0100

    Add "form builder" via external "Form Type" class.

--- src/AppBundle/Controller/ChoreController.php
+++ src/AppBundle/Controller/ChoreController.php
@@ -2,12 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\ChoreType;
 use Pimcore\Controller\FrontendController;
 use Pimcore\Model\DataObject\Chore;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class ChoreController extends FrontendController
@@ -17,16 +15,11 @@ class ChoreController extends FrontendController
      */
     public function defaultAction(Request $request)
     {
-        // creates a task object and initializes some data for this example
         $chore = new Chore();
         $chore->setChore('Write a blog post');
         $chore->setDueDate(new \DateTime('tomorrow'));
 
-        $form = $this->createFormBuilder($chore)
-            ->add('chore', TextType::class)
-            ->add('dueDate', DateType::class)
-            ->add('save', SubmitType::class, ['label' => 'Create Task'])
-            ->getForm();
+        $form = $this->createForm(ChoreType::class, $chore);
 
         return $this->render('Chore/default.html.twig', [
             'form' => $form->createView(),
